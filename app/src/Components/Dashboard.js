4a714e11a9a9874794f8f84db32c8b57ddd0cd34
Galo Sandoval <galosan92@gmail.com>
2020-12-02 16:27:46 -0800

    downloads dependencies

---
+++ app/src/Components/Dashboard.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import {
+  Button,
+  Chip,
+  List,
+  ListItem,
+  ListItemText,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: "50px",
+    padding: theme.spacing(3, 2),
+  },
+  flex: {
+    display: "flex",
+    alignItems: "center",
+  },
+  topicsWindow: {
+    width: "30%",
+    height: "300px",
+    borderRight: "1px solid grey",
+  },
+  chatWindow: {
+    width: "70%",
+    height: "300px",
+    padding: "20px",
+  },
+  chatBox: {
+    width: "85%",
+  },
+  button: {
+    width: "15%",
+  },
+  chip: {},
+}));
+
+export const Dashboard = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <Typography variant="h4" component="h4">
+          Chat App
+        </Typography>
+        <Typography variant="h5" component="h5">
+          Topic Placeholder
+        </Typography>
+        <div className={classes.flex}>
+          <div className={classes.topicsWindow}>
+            <List component="nav" aria-label="main mailbox folders">
+              {["topic"].map((topic) => (
+                <ListItem key={topic} button>
+                  <ListItemText primary={topic} />
+                </ListItem>
+              ))}
+            </List>
+          </div>
+          <div className={classes.chatWindow}>
+            {[{ from: "user", msg: "hello" }].map((chat, index) => (
+              <div className={classes.flex} key={index}>
+                <Chip label={chat.from} className={classes.chip} />
+                <Typography variant="p">{chat.msg}</Typography>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={classes.flex}>
+        <Button color="primary">Send</Button>
+        </div>
+      </Paper>
+    </div>
+  );
+};
