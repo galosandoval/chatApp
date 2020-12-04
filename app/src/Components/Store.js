1464007e6e0c55d9c63855ef921d8c4661001a0f
Galo Sandoval <galosan92@gmail.com>
2020-12-04 14:40:33 -0800

    adds store file with action, index, and reducer

--- app/src/Components/Store.js
+++ app/src/Components/Store.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
+const CTX = React.createContext()
+function reducer(state, action)PaymentResponse
 export const Store = () => {
+  const reducerHook = React.useReducer(reducer, initialState)
   return (
-    <div></div>
+    <CTX.Provider value={}>
+      {props.children}
+    </CTX.Provider>
   )
 }
